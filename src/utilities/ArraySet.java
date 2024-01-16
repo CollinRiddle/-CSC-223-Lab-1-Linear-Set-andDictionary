ac92b018c5091113a5279e6149e8f2ef2453c5f8
CollinRiddle <mr5542015@gmail.com>
2024-01-16 12:20:59 -0500

    ArraySet Implementation
    Auto-generated Required methods, deleted duplicates, and began basic return code for each method.

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -17,5 +17,136 @@ public class ArraySet<E> implements List<E>, Set<E>
 		_list = new ArrayList<E>();
 	}
 
+	@Override
+	public boolean add(E e) {
+		return _list.add(e);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		return _list.addAll(c);
+	}
+
+	@Override
+	public int size() {
+		return _list.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return _list.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return _list.contains(o);
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return _list.iterator();
+		}
+
+	@Override
+	public Object[] toArray() {
+		return _list.toArray();
+		}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return _list.toArray(a);
+	}
+
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
